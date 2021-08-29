ef47c12baf16c6d40455c148038170a4f1b2f6f9
Durrell Jules <durrelljules@unknown007c2d70c498.attlocal.net>
2021-08-29 19:26:23 -0400

    most dependencies installed, server working as intended

--- api/server.js
+++ api/server.js
@@ -1,1 +1,16 @@
 // build your server here and require it from index.js
+
+const express = require('express');
+
+// const recipeRouter = require('./recipes/recipe-router')
+ 
+const server = express();
+
+server.use(express.json());
+// server.use('/api/recipes', recipeRouter)
+
+server.use('/api/*', (_, res) => {
+  res.json({ data: 'will output to all endpoints'  });
+});
+
+module.exports = server
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // start your server here
+
+const server = require('./api/server')
+
+// Bring in dotenv with config method
+const dotenv = require('dotenv').config()
+
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => console.log(`*** Running on port ${port} ***`));
